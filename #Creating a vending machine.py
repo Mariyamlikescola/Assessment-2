59acfd2eb3508f71ea2dc94f4782ce12d23ada20
Mariyam Naeem <mariyamnaeem2706@gmail.com>
2025-01-19 00:39:49 +0400

    adding more items

--- #Creating a vending machine.py
+++ #Creating a vending machine.py
@@ -10,17 +10,20 @@ Menu_Inventory = {
     "Snacks": {
         "Takis": {"code": "S01", "price": 1.5, "stock": 10},
         "Cookie": {"code": "S02", "price": 2.0, "stock": 5},
-        "Snickers": {"code": "S03", "price": 1.75, "stock": 8},
+        "Snickers": {"code": "S03", "price": 3.0, "stock": 8},
+        "KitKat":{"code": "S04", "price": 3.0, "stock": 7},
     },
     "Drinks": {
         "Water": {"code": "D01", "price": 1.0, "stock": 20},
-        "Coca Cola": {"code": "D02", "price": 1.25, "stock": 15},
-        "Mango juice": {"code": "D03", "price": 1.5, "stock": 12},
+        "Coca Cola": {"code": "D02", "price": 2.5, "stock": 15},
+        "Mango juice": {"code": "D03", "price": 2.0, "stock": 12},
+        "Seven up":{"code": "D04", "price": 2.5, "stock": 15},
     },
     "Healthy Options": {
         "Energy Bar": {"code": "H01", "price": 2.0, "stock": 7},
         "Pretzels": {"code": "H02", "price": 2.5, "stock": 5},
         "Fruit Cup": {"code": "H03", "price": 3.0, "stock": 6},
+        "Banana": {"code": "H04", "price": 2.0, "stock": 6},
     },
 }
 
